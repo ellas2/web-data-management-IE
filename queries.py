b793f93d78dc7861957669e0e0e1df6d6ab806cf
ellas2 <ella.steinberg93@gmail.com>
2019-05-18 20:48:33 +0300

    fixed queries - they now work :D

--- queries.py
+++ queries.py
@@ -9,22 +9,22 @@ def run_query(graph, query, query_num):
     for row in x1:
         print (row)
         count+=1
-    print('num rows is ' + count)
+    print('num rows is ' + str(count))
 
 # All prime ministers
-q_a = "select DISTINCT (?a) where { ?country <http://example.org/prime_minister> ?a }"
+q_a = "select ?a where { ?country <http://en.wikipedia.org/prime_minister> ?a }"
 
 # All countries
-q_b = "select ?country where { ?country <http://example.org/area> ?b }"
+q_b = "select ?country where { ?country <http://en.wikipedia.org/area> ?b }"
 #TODO: this should maybe be changed
 
 # All countries that are republics
-q_c = "select ?country where { ?country <http://example.org/government> ?b." \
-      "FILTER (contains(str(?b), republic)) }"
+q_c = "select ?country where { ?country <http://en.wikipedia.org/government> ?b." \
+      "FILTER (contains(str(?b), \"republic\")) }"
 
 # All countries that are monarchies
-q_d = "select ?country where { ?country <http://example.org/government> ?b." \
-      "FILTER (contains(str(?b), monarchy)) }"
+q_d = "select ?country where { ?country <http://en.wikipedia.org/government> ?b." \
+      "FILTER (contains(str(?b), \"monarchy\")) }"
 
 graph = rdflib.Graph()
 run_query(graph, q_a, '1')
